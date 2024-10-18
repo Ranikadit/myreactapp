4584d92a046ac30293a0ebb0a1e22bc3185c1e69
Ranikadit <vmranimca@gmail.com>
2024-10-18 11:57:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Doctor Booking App</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/UserList.js
@@ -0,0 +1,30 @@
+// src/UserList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/findAll')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>  // Adjust 'name' based on your User entity
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
